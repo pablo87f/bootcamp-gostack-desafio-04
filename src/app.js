550c47a46a1bd63dc46d165f5d81c55c69ab80de
Pablo Fernandes <pablofern87@gmail.com>
2020-02-26 10:46:59 -0300

    feat(configurando o projeto) - commit inicial com as configurações básicas do projeto

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css'
+import profile from './assets/perfil-pablo.jpeg'
+
+function App() {
+
+    return (
+        <>
+            <h1>Ola mundo</h1>
+            <img src={profile} width={200}></img>
+        </>
+    )
+}
+
+export default App; 
